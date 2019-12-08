b62e9f527521fda102a94db6cf34c847e67b1d57
dannyl <danilovlaurent@pursuit.org>
2019-12-08 17:49:57 -0500

    sections divided

---
+++ index.js
@@ -0,0 +1,37 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     let blue = document.querySelector(".blue")
+//     let white = document.querySelector(".white")
+//     let footer = document.querySelector(".footer")
+    
+//     let blueUl = document.createElement("ul")
+//     blueUl.style.listStyleImage = "url('sqorange.gif')"
+//     let blueLi1 = document.createElement("li")
+//     let blueLi2 = document.createElement("li")
+//     let blueLi3 = document.createElement("li")
+
+//     blueUl.appendChild(blueLi1)
+//     blueLi1.innerText = "Follow your interests"
+//     blueUl.appendChild(blueLi2)
+//     blueLi2.innerText = "Hear what people are talking about"
+//     blueUl.appendChild(blueLi3)
+//     blueLi3.innerText = "Join the conversation"
+ 
+//     blue.appendChild(blueUl)
+
+//     let whiteForm = document.createElement("form")
+//     let whiteInput = document.createElement("input")
+//     let whiteInput2 = document.createElement("input")
+
+//     whiteInput.placeholder = "Phone,Email,or Username"
+//     whiteInput2.placeholder = "Password"
+    
+    
+//     whiteForm.appendChild(whiteInput)
+//     whiteForm.appendChild(whiteInput2)
+
+//     white.appendChild(whiteForm)
+
+
+    
+    
+// })
